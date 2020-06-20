bfa4da6aae6e6e0e90662cbf259c4a579faea88b
Hugh Shanahan <Hugh.Shanahan@rhul.ac.uk>
2020-06-20 13:16:26 +0100

    rewrote getWebPages

--- downloadAllSites.py
+++ downloadAllSites.py
@@ -8,13 +8,16 @@ Created on Wed Jun  3 16:18:51 2020
 
 import os
 import requests
+from requests.exceptions import HTTPError
+from requests.exceptions import Timeout
+
 import json
 
 """
 input fn 
 creates a file (filename fn) with a list of repo ids from r3data
 """
-def downloadRepos(fn):
+def downloadRepos(fn='../repos/repos.txt'):
     command = 'curl -o r.xml https://www.re3data.org/api/v1/repositories; grep \<id\> r.xml > '+fn+';rm -f r.xml'
     os.system(command)
 
@@ -23,7 +26,7 @@ def downloadRepos(fn):
 input repoFn
 output list of tuples, each tuple is (repoId, repoURL)
 """
-def collateRepoURLs(repoFn):
+def collateRepoURLs(repoFn='../repos/repos.txt'):
     repos = []
     with open(repoFn) as f:
         for line in f:
@@ -43,15 +46,15 @@ def collateRepoURLs(repoFn):
 input filename
 output repos list of tuples of form (r3dID, url)
 """
-def readReposList(filename):
+def readReposList(filename="../repos/repos.json"):
     with open(filename) as f:
-        return(json.load(filename))
+        return(json.load(f))
     
 """
 input filename, repos list of tuples of form (r3dID, url)
 saves repos as a json file
 """    
-def writeReposList(repos,filename):
+def writeReposList(repos,filename="../repos/repos.json"):
     with open(filename, 'w') as json_file:
         json.dump(repos,json_file)
 
@@ -59,10 +62,40 @@ def writeReposList(repos,filename):
 input repos - list of repos (tuples (r3dID, url))
 downloads web pages corresponding to the list of repos.
 """        
-def getWebPage(repos):
-    for repo in repos:
-        command = 'curl -o r3d/'+repo[0]+'.html '+repo[1]
-        os.system(command)
+def getWebPage(repos,jsonFnRoot="../r3d/",TIMEOUT=30):
+    for (r,url) in repos:
+        print(r + " " + url)
+        responseData = {}
+# Set up session so that request looks as if it is from a standard browser
+        http = requests.Session()
+        http.headers.update({"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0"
+})
+        try:
+# Try and perform get, if there is an error or timeout, record that information            
+            response = http.get(url,timeout=TIMEOUT)
+            response.raise_for_status()
+        except Timeout:
+            print("Timeout")
+            responseData['Timeout'] = True    
+        except HTTPError as http_err:
+            responseData['HTTPError'] = str(http_err)
+        except Exception as err:
+            responseData['otherErr'] = str(err)
+
+            
+        else:
+            responseData['headers'] = dict(response.headers)
+            responseData['text'] = response.text
+            responseData['status_code'] = response.status_code
+        
+        jsonFn = jsonFnRoot + r + ".json"
+
+        with open(jsonFn, 'w') as json_file:
+            json.dump(responseData,json_file)
+        
+            
+        
+
 
         
         

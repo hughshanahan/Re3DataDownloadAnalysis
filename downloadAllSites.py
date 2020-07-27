3f53625d556c380c03ad060c41f22abad838ea9d
Hugh Shanahan <Hugh.Shanahan@rhul.ac.uk>
2020-07-27 19:19:52 +0100

    git message removed

--- downloadAllSites.py
+++ downloadAllSites.py
@@ -166,7 +166,7 @@ def makeDir(newDir):
 # Check if newDir exists otherwise create it
     p = Path(newDir)
     if not p.exists():
-        os.mkdir(newDir
+        os.mkdir(newDir)
     
 """
 input oldDir - directory with repo json files
@@ -226,7 +226,7 @@ repoFn is read and the list of repos downloaded in the root directory in folder 
 an hour later the repos that are timed out are rerun in folder 1 and so on until we
 get to folder maxIter-1.
 """
-<<<<<<< HEAD
+
     
 def fullRun(repoFn,rootDir,maxIter=20):
     repos = readReposList(repoFn)
@@ -240,23 +240,7 @@ def fullRun(repoFn,rootDir,maxIter=20):
     oldDir = os.path.join(rootDir,str(iter))
     makeDir(oldDir)
     
-=======
-    
-def fullRun(repoFn,rootDir,maxIter=20):
-    repos = readReposList(repoFn)
-    iter = 0
-    allDone = False
-    
-    print("Analysing for ",rootDir)
-    makeDir(rootDir)
-
-    print("Starting initial run")
-    oldDir = os.path.join(rootDir,str(iter))
-    makeDir(oldDir)
-    
->>>>>>> efbd26e2049e546f7e67df62e04bf2b2d98bd8dd
     getWebPage(repos, jsonFnRoot = oldDir)
-
     
     while  not allDone and iter < maxIter:
         iter += 1

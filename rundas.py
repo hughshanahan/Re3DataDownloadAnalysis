81bf042e6ad404cfa11b4399486a71f32c375178
Hugh Shanahan <Hugh.Shanahan@rhul.ac.uk>
2020-08-05 10:28:16 +0100

    Script for running downloadAllSites functions

---
+++ rundas.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 09:38:07 2020
+
+@author: upac004
+"""
+
+import downloadAllSites as das
+
+das.myConfig = das.getConfigData()
+
+country = 'gb'
+nPort = das.openPort(country)
+
+print("Country = "+das.countryForPort(nPort))
+
+pData = das.proxyData(nPort)
+
+print(pData)
+
+das.closePort(nPort)
+
+
+
+
+
+
+

29dc724b30f9209be3d30ac46f0f2dc9e43e5fa9
Hugh Shanahan <hugh.shanahan@cs.rhul.ac.uk>
2020-08-07 16:21:29 +0000

    updated script

--- rundas.py
+++ rundas.py
@@ -7,22 +7,36 @@ Created on Wed Aug  5 09:38:07 2020
 """
 
 import downloadAllSites as das
+import json
+import os
 
 das.myConfig = das.getConfigData()
 
 country = 'jp'
-nPort = das.openPort(country)
 
-print("Country = "+das.countryForPort(nPort))
+re3 = das.readReposList("./repos.json")
+additional = das.readReposList("./additionalSites.json")
 
-pData = das.proxyData(nPort)
-
-print(pData)
-
-das.closePort(nPort)
+path = "../data"
 
 for country in das.countriesList:
+    nPort = das.openPort(country)
+
+    print("Country = "+das.countryForPort(nPort))
+
+    pData = das.proxyData(nPort)
+
+    dataPath = os.path.join(path,country)
+    das.makeDir(dataPath)
     
+    portDataFn = os.path.join(dataPath,"portData.json") 
+    with open(portDataFn,"w") as pDataOutFile:
+        pDataOutFile.write(json.dumps(pData))
+        
+    das.getWebPage(re3,dataPath)
+    das.getWebPage(additional,dataPath)    
+
+    das.closePort(nPort)    
 
 
 

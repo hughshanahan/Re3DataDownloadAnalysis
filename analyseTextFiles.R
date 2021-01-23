34dfffe6ffb015eb1f5f9ae7e050f36ebc1dbeac
Hugh Shanahan <Hugh.Shanahan@rhul.ac.uk>
2021-01-23 00:59:10 +0000

    Corrected some errors

--- analyseTextFiles.R
+++ analyseTextFiles.R
@@ -1,22 +1,27 @@
-stringDist <- function(s1,s2){
+stringDistance <- function(s1,s2){
   require(stringdist)
   l = nchar(s1) + nchar(s2)
-  stringdist(s1,s2,method='lcs')
+  s1pr <- gsub("[[:punct:]]","",s1)
+  s2pr <- gsub("[[:punct:]]","",s2)
+  stringdist(s1pr,s2pr,method='cosine',q=3)
 }
 
 cmpLists <- function(l1,l2){
   d = 0
+#  print(l1)
   s1 = ifelse('text' %in% names(l1),l1$text,-1)
   s2 = ifelse('text' %in% names(l2),l2$text,-10) 
-  if (typeof(s1) == 'character' && typeof(s2) == 'character'){
-      d = stringDist(s1,s2)
+  t1 <- typeof(s1) == 'character'
+  t2 <- typeof(s2) == 'character'
+  if (t1 && t2){
+      d = stringDistance(s1,s2)
   }
   else{
-    if ( typeof(s1) == 'character' ){
+    if ( t1  ){
       d = s2
     }
     else {
-      if ( typeof(s2) == 'character' ){
+      if ( t2  ){
         d = s1
       }
       else{
@@ -39,12 +44,38 @@ cmpFolders <- function(p1,p2){
   files2 = dir(path=p2,pattern=".json")
   files = files1[which(files1 %in% files2)]
   
-  results = list()
+  results = c()
   for ( f in files ){
     f1 = paste(p1,f,sep="/")
     f2 = paste(p2,f,sep="/")
-    results[f] = cmpJsonFiles(f1,f2)
+    results = c(results,cmpJsonFiles(f1,f2))
   }
+  names(results) <- files
+  print(results)
   results
+}
+
+runAllFolders <- function(myFolder){
+  allFolders <- dir(myFolder)
+  N <- length(allFolders)
+#  for ( i in seq(1,N-1) ){
+#    for ( j in seq(i+1,N) ){
+#      fi <- allFolders[i]
+#      fj <- allFolders[j]
+  for ( fi in allFolders ){
+    thisData <- list()
+    for ( fj in allFolders ){
+      if ( fi != fj ){
+        print(c(fi,fj))
+        x <- cmpFolders(fi,fj)
+        thisData$country1 <- fi
+        thisData$country2 <- fj
+        thisData$data <- x
+        fn <- paste("/Users/upac004/Downloads/geoblock_",fi,"_",fj,".Rdata",sep="")
+        save(thisData,file = fn)
+      }  
+    }
+  }
+  
 }
 
